1cdea1bb3e98e75d4243655ef6a62fd30d6ef975
katayama <kataponess1@gmail.com>
2019-11-06 13:58:26 +0900

    commit

--- wp-content/themes/cocoon-master/tmp/body-top.php
+++ wp-content/themes/cocoon-master/tmp/body-top.php
@@ -5,7 +5,7 @@
  * @link: https://wp-cocoon.com/
  * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
  */
-if ( !defined( 'ABSPATH' ) ) exit; ?>
+if (!defined('ABSPATH')) exit; ?>
 
 <div id="container" class="container<?php echo get_additional_container_classes(); ?> cf">
   <?php //サイトヘッダー
@@ -27,21 +27,21 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
   ////////////////////////////
   //コンテンツ上部ウィジェット
   ////////////////////////////
-  if ( is_active_sidebar( 'content-top' ) ) : ?>
-  <div id="content-top" class="content-top">
-    <div id="content-top-in" class="content-top-in wrap">
-      <?php dynamic_sidebar( 'content-top' ); ?>
+  if (is_active_sidebar('content-top')) : ?>
+    <div id="content-top" class="content-top">
+      <div id="content-top-in" class="content-top-in wrap">
+        <?php dynamic_sidebar('content-top'); ?>
+      </div>
     </div>
-  </div>
   <?php endif; ?>
 
   <?php //投稿パンくずリストがメイン手前の場合
-  if (is_single() && is_single_breadcrumbs_position_main_before()){
+  if (is_single() && is_single_breadcrumbs_position_main_before()) {
     get_template_part('tmp/breadcrumbs');
   } ?>
 
   <?php //固定ページパンくずリストがメイン手前の場合
-  if (is_page() && is_page_breadcrumbs_position_main_before()){
+  if (is_page() && is_page_breadcrumbs_position_main_before()) {
     get_template_part('tmp/breadcrumbs-page');
   } ?>
 
@@ -52,4 +52,4 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
 
     <div id="content-in" class="content-in wrap">
 
-        <main id="main" class="main<?php echo get_additional_main_classes(); ?>" itemscope itemtype="https://schema.org/Blog">
+      <main id="main" class="main<?php echo get_additional_main_classes(); ?>" itemscope itemtype="https://schema.org/Blog">
